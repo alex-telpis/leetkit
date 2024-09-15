9d72d22a5d52078ce41c9c661eb9c5815aa39c79
Alex Telpis <alex.telpis@gmail.com>
2024-09-15 18:52:26 -0500

    Fix issue with verification involving complex DS

--- verify.go
+++ verify.go
@@ -7,11 +7,23 @@ import (
 // Verify checks if the result is equal to the expected value and prints the outcome.
 // Use this function to validate your solution against test cases.
 func Verify(expect any, result any) {
-	res, exp := Sprint(result), Sprint(expect)
+	exp, res := prepareVerify(expect, result)
 	if res == exp {
 		fmt.Printf("    ✅ Pass. TestCase: %s\n", exp)
 		return
 	}
 
 	fmt.Printf("    ❌ Fail. Expected: %s, but got: %s\n", exp, res)
+}
+
+func prepareVerify(expect any, result any) (string, string) {
+	if exp, isStr := expect.(string); isStr {
+		if res, isStr := result.(string); isStr {
+			return exp, res
+		}
+
+		return exp, Sprint(result)
+	}
+
+	return Sprint(expect), Sprint(result)
 }
